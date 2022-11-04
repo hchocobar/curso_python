4d34f8fa61dbcd5a97d3ae15e18694d4a0c08ee4
hchocobar <hector@chocobar.net>
2022-11-04 08:31:30 -0300

    refactor (labs exercises ): create new directory
    directory labs
    directory exercises
    files rename with unit prefix

---
+++ py02_intro/comentarios_segundos.py
@@ -0,0 +1,11 @@
+# este programa calcula los segundos en cierto número de horas determinadas
+horas = 2  # numero de horas
+segundos_hora = 3600  # número de segundos en una hora
+
+print("Horas:", horas)  # imprime el número de horas
+print("En", horas, "horas, hay:", horas * segundos_hora, 'segundos.')  # imprime la cantidad de segundos en las horas
+
+"""
+Esto es un comentario que abarca múltiples líneas.
+Aquí tenemos el fin del programa que calcula el número de segundos en 2 horas
+"""
--- py04_colecciones/diccionarios_02_accediendo.py
+++ py04_colecciones/diccionarios_02_accediendo.py
@@ -1,13 +1,17 @@
-# Si se deseamos acceder a un elemento del diccionario, se puede hacer haciendo
-# referencia a su clave colocándola dentro de corchetes (ejemplo 1) o utilizando
-# el método get() (ejemplo 2):
-miDiccionario = {
+# Si deseamos acceder a un elemento del diccionario, se puede hacer haciendo
+mi_diccionario = {
     "fire": "fuego",
     "water": "agua",
     "earth": "tierra",
     "wind": "viento",
 }
+
+
+# 1. referencia a su clave colocándola dentro de corchetes (ejemplo 1)
 elemento1 = miDiccionario["water"]  # haciendo referencia a su clave dentro de corchetes
 print(elemento1)  # salida: agua
+
+
+# 2. utilizando el método get()
 elemento2 = miDiccionario.get("earth")  # utilizando el método get()
 print(elemento2)  # salida: tierra
