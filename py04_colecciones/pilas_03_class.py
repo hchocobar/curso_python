06623c4f4c57859a5d536fa75ddb6566c6fc48f5
Hector Chocobar-Torrejon <hector@chocobar.net>
2023-05-24 09:10:49 -0300

    review: py04 - pilas

--- py04_colecciones/pilas_03_class.py
+++ py04_colecciones/pilas_03_class.py
@@ -1,9 +1,9 @@
 # Implementación de una Pila mediante una clase
 class Pila:
-    def __init__(self):  # constructor de la clase
-        self.__lista_pila = []  # definimos una variable privada (pseudo-encapsulamos)
+    def __init__(self):  # Constructor de la clase
+        self.__lista_pila = []  # Definimos una variable privada (pseudo-encapsulamos)
 
-    def push(self, val):  # el parámetro self apunta a la clase misma, Python lo agregará siempre como primer argumento
+    def push(self, val):  # El parámetro self apunta a la clase misma, Python lo agregará siempre como primer argumento
         self.__lista_pila.append(val)
 
     def pop(self):
@@ -12,14 +12,13 @@ class Pila:
         return val
 
 
+'''Nota: Faltaría implementar un try/except para evitar el error que ocasionaría un pop() en una pila vacía'''
 objeto_pila = Pila()
 
-objeto_pila.push(3)  # el argumento self no se envía, Python lo agregará siempre
+objeto_pila.push(3)  # El argumento self no se envía, Python lo agregará siempre
 objeto_pila.push(2)
 objeto_pila.push(1)
 
 print(objeto_pila.pop())
 print(objeto_pila.pop())
 print(objeto_pila.pop())
-
-'''Nota: Faltaría implementar un try/except para evitar el error que ocasionaría un pop() en una pila vacía'''
