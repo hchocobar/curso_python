49a310e7b824c81684f4e2a69501febe6d6192c2
Hector Chocobar-Torrejon <hector@chocobar.net>
2023-06-03 15:56:07 -0300

    review: py09 - proyectos básicos

--- py09_proyectos_basicos/01_fechas/enunciado_01.py
+++ py09_proyectos_basicos/01_fechas/enunciado_01.py
@@ -1,6 +1,7 @@
 # Laboratorio 1
-# Tu tarea es escribir y probar una función que toma un argumento (un año) y devuelve True si el año es un año bisiesto, o False sí no lo es.
-# Desde la introducción del calendario gregoriano (en 1582), se utiliza la siguiente regla para determinar el tipo de año:
+# Tu tarea es escribir y probar una función que toma un argumento (un año) y devuelve True si el año es un año bisiesto,
+# o False sí no lo es.
+# Desde la introducción del calendario gregoriano (1582), se utiliza la siguiente regla para determinar el tipo de año:
 # - Si el número del año no es divisible entre cuatro, es un año común.
 # - De lo contrario, si el número del año no es divisible entre 100, es un año bisiesto.
 # - De lo contrario, si el número del año no es divisible entre 400, es un año común.
@@ -13,9 +14,12 @@
 #     • la otra con los resultados esperados.
 # El código te dirá si alguno de tus resultados no es válido.
 def is_year_leap(year):
-#
-# coloca tu código aquí
-#
+    pass
+    #
+    # Coloca tu código aquí
+    #
+    return
+
 
 test_data = [1900, 2000, 2016, 1987]
 test_results = [False, True, True, False]
@@ -27,4 +31,3 @@ for i in range(len(test_data)):
         print("OK")
     else:
         print("Error")
-
